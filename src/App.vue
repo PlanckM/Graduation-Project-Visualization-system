cfc7b6f5ce5d23278e1b84c49f456ae8f13d4eba
guxuankun <12953853+guxuankun@user.noreply.gitee.com>
2025-03-11 16:51:00 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="cesiumContainer" ref="cesiumContainer"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import * as Cesium from "cesium";
+import "cesium/Build/Cesium/Widgets/widgets.css"; // 从 node_modules 引入 Cesium 的 CSS
+import { onMounted } from 'vue';
+
+// 设置 Cesium 的静态资源路径
+window.CESIUM_BASE_URL = "/";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  setup() {
+    onMounted(() => {
+      console.log("Cesium Container:", document.getElementById("cesiumContainer"));
+      var viewer = new Cesium.Viewer("cesiumContainer");
+    });
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+}
+#cesiumContainer {
+  width: 100vw;
+  height: 100vh;
 }
 </style>
